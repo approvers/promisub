e93ff64e3bcc8e411ce6163ae569eb2a277cdf50
MikuroXina <ryosukadnak@gmail.com>
2020-07-26 16:44:46 +0900

    :pencil: Add document

--- mod.ts
+++ mod.ts
@@ -1,16 +1,54 @@
+/**
+ * Promisub provides pub/sub features.
+ * 
+ *    const p = new Promisub<number>();
+ * 
+ *    // Publish events
+ *    p.publish(event1, event2, ...);
+ * 
+ *    // Wait an event
+ *    await p.once();
+ * 
+ *    // Listen events infinite
+ *    for await (const event of p) {
+ *      // Handle event here
+ *      // :
+ *      // :
+ * 
+ *      // Stop to listen
+ *      p.close();
+ *    }
+ */
 export class Promisub<T> implements AsyncIterable<T> {
   private events: T[] = [];
   private closed = false;
 
+  /**
+   * `publish` queues events.
+   *
+   * @param {...T[]} events
+   * @memberof Promisub
+   */
   publish(...events: T[]) {
     this.events.push(...events);
   }
 
+  /**
+   * `close` cancels listening all.
+   *
+   * @memberof Promisub
+   */
   close() {
     this.closed = true;
     this.events = [];
   }
 
+  /**
+   * `once` takes an event.
+   *
+   * @returns {Promise<T>}
+   * @memberof Promisub
+   */
   async once(): Promise<T> {
     while (!this.closed) {
       const head = this.events.shift();
@@ -21,6 +59,12 @@ export class Promisub<T> implements AsyncIterable<T> {
     throw new Error("the subscription was closed");
   }
 
+  /**
+   * `Symbol.asyncIterator` listens events.
+   *
+   * @returns {AsyncIterableIterator<T>}
+   * @memberof Promisub
+   */
   async *[Symbol.asyncIterator](): AsyncIterableIterator<T> {
     while (!this.closed) {
       const head = this.events.shift();
